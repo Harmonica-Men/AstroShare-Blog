cdc25d5fbfb80544e0c70a7d74f328e9590b7e61
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 06:20:29 +0000

    Change order of html in urls.py

--- blogger/urls.py
+++ blogger/urls.py
@@ -3,8 +3,8 @@ from .views import FrontpageView, HomepageView, ArticleDetailView, AddPostView
 
 
 urlpatterns = [
-    path('', HomepageView, name= 'homepage'),
-    path('frontpage/', FrontpageView.as_view(), name= 'frontpage-blogpost'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_newpost/', AddPostView.as_view(), name='add-newpost'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('frontpage/', FrontpageView.as_view(), name= 'frontpage-blogpost'),
+    path('', HomepageView, name= 'homepage'),
 ]
