6edaaa98d5731d4c8a314127f3ed4786bdc7277e
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 09:29:36 +0000

    Models.py Add post_date field

--- blogger/models.py
+++ blogger/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     title_tag = models.CharField(max_length=200, default= "star gazer")
     author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
