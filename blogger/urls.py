2929c7cdbf62de36d2454b516fc3fecd3d4d51ba
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 09:04:04 +0000

    Fix bug UpdatePostView

--- blogger/urls.py
+++ blogger/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('add_newpost/', AddPostView.as_view(), name='add-newpost'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('frontpage/', FrontpageView.as_view(), name= 'frontpage-blogpost'),
-    path('article/<int:pk>', UpdatePostView.as_view(), name= 'update-post')    
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= 'update-post')    
 ]
