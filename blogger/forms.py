5ca7d4ea2d7aa7beea4ea82aa21da9bc2bbb2f8e
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 08:35:01 +0000

    Fix issue bootstrap from-group in 'add_post.html'

--- blogger/forms.py
+++ blogger/forms.py
@@ -1,15 +1,14 @@
 from django import forms
 from .models import Post
 
-class PostForm(format.ModelForm):
+class PostForm(forms.ModelForm):
     class Meta: 
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ['title', 'title_tag', 'author', 'body']
 # widgits are magic
         widgets = {
-            'title' : forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'placeholder test'}),
+            'title' : forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
             'author' : forms.TextInput(attrs={'class': 'form-control'}),
-            'category' : forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body' : forms.Textarea(attrs={'class': 'form-control'}),            
         }
--- blogger/views.py
+++ blogger/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.urls import reverse_lazy
+# from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 from .forms import PostForm
