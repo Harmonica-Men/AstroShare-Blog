6fcb5a7915d865979220cee79e37bbde4d31b554
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 09:49:17 +0000

    Set up urls.py for members

--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
-urlpatterns = []
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(blogger.urls)),
+    path('members/', include('django.contrib.auth.url')),
+    path('members/', include('members.urls')),
+]
   
