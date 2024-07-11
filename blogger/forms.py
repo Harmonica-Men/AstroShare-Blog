8589e42703c1155ea389a84d468c2d6c3d3a5061
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 20:05:35 +0000

    Add 'js_id' javascript to retrive user.id

--- blogger/forms.py
+++ blogger/forms.py
@@ -9,6 +9,7 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title' : forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
-            'author' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'user name'}),
+            'author' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'user name', 'id': 'js_id', 'type': 'hidden'}),
+            #'author' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'user name'}),
             'body' : forms.Textarea(attrs={'class': 'form-control'}),            
         }
