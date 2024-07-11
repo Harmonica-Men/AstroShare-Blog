173a2ee83ca4c72ad14e037b972df341f7a21643
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 09:16:47 +0000

    Test DeletePostView

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import FrontpageView, HomepageView, ArticleDetailView, AddPostView, UpdatePostView
+from .views import FrontpageView, HomepageView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 
 urlpatterns = [
     path('', HomepageView, name= 'homepage'),
     path('add_newpost/', AddPostView.as_view(), name='add-newpost'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('frontpage/', FrontpageView.as_view(), name= 'frontpage-blogpost'),
-    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= 'update-post')    
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= 'update-post'),
+    path('article/<int:pk>/delete', DeletePostView.as_view(), name= 'delete-post')    
 ]
