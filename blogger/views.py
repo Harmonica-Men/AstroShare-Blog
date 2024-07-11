067f2735d8d5069bd4093ce8d4b623d11b3cd926
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 08:04:37 +0000

    Changes in AddPostview define PostForm

--- blogger/views.py
+++ blogger/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 def HomepageView(request): # Homepage view
     return render(request, 'homepage.html')
@@ -17,6 +18,9 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__' # because I am lazy
+
+
+    # fields = '__all__' # because I am lazy
     # success_url = reverse_lazy('frontpage-blogpost')  # go back to frontpage of blogpost
