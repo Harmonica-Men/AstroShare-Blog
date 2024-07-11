f34fb9bedae468bdcd09d60bc28f58dad024ec7a
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-11 05:43:38 +0000

    AdCheck navigation Navbar

--- AstroShareBlog/urls.py
+++ AstroShareBlog/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blogger import views 
-
+from blogger import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
