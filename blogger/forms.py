7671ed2de538190383fbed1283e6fbb05fe9c237
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-12 05:35:46 +0000

    Populate Category hardecode list

--- blogger/forms.py
+++ blogger/forms.py
@@ -1,15 +1,20 @@
 from django import forms
 from .models import Post
 
+# Hardcode the list
+choices = [('coding', 'coding'), ('sports', 'sports'), ('voetbal', 'voetbal')]
+
 class PostForm(forms.ModelForm):
     class Meta: 
         model = Post
-        fields = ['title', 'title_tag', 'author', 'body']
+        fields = ['title', 'title_tag', 'author', 'category', 'body']
 # widgits are magic
         widgets = {
             'title' : forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs={'class': 'form-control'}),
             'author' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'user name', 'id': 'js_id', 'type': 'hidden'}),
             #'author' : forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'user name'}),
+            #'category' : forms.TextInput(attrs={'class': 'form-control'}),
+            'category' : forms.Select(choices=choices, attrs={'class': 'form-control'}),
             'body' : forms.Textarea(attrs={'class': 'form-control'}),            
         }
