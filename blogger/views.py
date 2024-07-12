1009bad1666f93b861ce73c77a1bcfb39d974e30
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-12 06:34:26 +0000

    Fix bug whit the 'get_object_or_404'

--- blogger/views.py
+++ blogger/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
+from django.http import Http404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views import View
