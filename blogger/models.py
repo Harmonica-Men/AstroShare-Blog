3f5f4abe30832e7b861f8b8cafacc7fc43e67337
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-12 05:25:47 +0000

    Create in Models.py (blogger) class Category and migrate

--- blogger/models.py
+++ blogger/models.py
@@ -3,12 +3,25 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from datetime import datetime, date
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+#     # title = models.CharField(max_length=255)
+#     # slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('frontpage-blogpost') 
+        
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     title_tag = models.CharField(max_length=200, default= "star gazer")
     author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
     body = models.TextField()
     post_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=200, default='coding')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
