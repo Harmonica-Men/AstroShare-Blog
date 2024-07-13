052e2aafefb009ee9a7e1973220b638e4a2c3c69
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-13 10:03:52 +0000

    Add forms.py in member and move content of SignUpForm in forms.py of Blogger to new forms.py (blogger)

--- blogger/forms.py
+++ blogger/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post, Category
 
+
 # Hardcode the list
 # choices = [('coding', 'coding'), ('sports', 'sports'), ('voetbal', 'voetbal')]
 choices = Category.objects.all().values_list('name', 'name')
@@ -20,18 +21,4 @@ class PostForm(forms.ModelForm):
             'body' : forms.Textarea(attrs={'class': 'form-control'}),            
         }
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder' : 'my@somedomain.org'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    
-    class Meta:
-        model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
 
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        # widget doc django
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password1'].widget.attrs['class'] = 'form-control'
-        self.fields['password2'].widget.attrs['class'] = 'form-control'
