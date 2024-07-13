00c1f11069afcc2b77cf714d1f09c53f4f6a4c02
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-13 10:06:51 +0000

    Test the SignUpForm

--- members/forms.py
+++ members/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import forms
+
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder' : 'my@somedomain.org'}))
