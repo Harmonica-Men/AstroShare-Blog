2cf0c5ae208919827078462c4e69dcdb6ba33411
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-13 09:58:24 +0000

    Create SignUpForm

--- blogger/forms.py
+++ blogger/forms.py
@@ -19,3 +19,19 @@ class PostForm(forms.ModelForm):
             'category' : forms.Select(choices=choices, attrs={'class': 'form-control','placeholder': 'choice'}),
             'body' : forms.Textarea(attrs={'class': 'form-control'}),            
         }
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder' : 'my@somedomain.org'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+        # widget doc django
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
