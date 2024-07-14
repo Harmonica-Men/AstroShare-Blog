8503bcb5b8b95f3a7a6c9776111c683d372ed7a9
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-14 13:45:33 +0000

    Configure settings.py cloudinary (whit env.py)

--- AstroShareBlog/settings.py
+++ AstroShareBlog/settings.py
@@ -10,10 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+
+os.environ['CLOUDINARY_CLOUD_NAME'] = 'dtbji5cfz'
+os.environ['CLOUDINARY_API_KEY'] = '839441162297935'
+os.environ['CLOUDINARY_API_SECRET'] = 'eTQFu1X9X065YP1jycD8d4aMSJc'
+                                      
+
+from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-from pathlib import Path
 # from cloudinary.models import CloudinaryField
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,7 +37,9 @@ DATABASE_URL = 'postgres://uwb4hdjzua9:7p5Hb6Ee1XKE@ep-gentle-mountain-a23bxz6h.
 
 SECRET_KEY = 'django-insecure-m@=5yl8r(&a%ez6c5#le$5%za(cqckq#53bvnl^)24-p3zgobe'
 
-CLOUDINARY_URL = "cloudinary://839441162297935:eTQFu1X9X065YP1jycD8d4aMSJc@dtbji5cfz"
+# CLOUDINARY_URL = "cloudinary://394961391133235:Pj9-5H6dpGHJbBmra5wtJ2ci_OQ@dtbji5cfz"
+CLOUDINARY_URL = "CLOUDINARY_URL"
+# CLOUDINARY_URL = "cloudinary://839441162297935:eTQFu1X9X065YP1jycD8d4aMSJc@dtbji5cfz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
