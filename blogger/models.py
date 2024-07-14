587e953fd9e65a57e2594b768d7e7b43b124fcb5
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-14 10:12:42 +0000

    Ainstall package cloudenary

--- blogger/models.py
+++ blogger/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 from datetime import datetime, date
+from cloudinary.models import CloudinaryField
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
