7f0e7299861c33a22aa57f6ad5fa278a1ebfaea1
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-07-15 11:56:18 +0000

    Add in views/py EditProfilePageView

--- members/urls.py
+++ members/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, CustomPasswordChangeView, ShowProfilePageView
+from .views import UserRegisterView, UserEditView, CustomPasswordChangeView, ShowProfilePageView, EditProfilePageView
 #from django.contrib.auth import views as auth_views
 
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('<int:pk>/password/', CustomPasswordChangeView.as_view(), name='password-change'),
     path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show-profile-page'),
-    # path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
-        ]
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit-profile-page'),
+    ]
