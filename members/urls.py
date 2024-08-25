4cab6000ed36e0d2f6ca834b03da235ca086bf81
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-08-25 07:21:49 +0000

    Fix bug login html & Search bar layout

--- members/urls.py
+++ members/urls.py
@@ -9,6 +9,7 @@ from django.contrib.auth.views import PasswordChangeView
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='homepage.html'), name='home'),  
     path('register/', UserRegisterView.as_view(), name='register'),
+   
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('<int:pk>/password/', CustomPasswordChangeView.as_view(), name='password-change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
