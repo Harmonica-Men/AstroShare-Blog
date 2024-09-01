19284251b2ecb752cc118d485039f6dc65a74476
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-09-01 20:17:33 +0000

    Remove title_tag form models.py + check all associate files + migrate

--- blogger/forms.py
+++ blogger/forms.py
@@ -6,10 +6,14 @@ class PostForm(forms.ModelForm):
     
     class Meta:
         model = Post
-        fields = ['title', 'title_tag', 'category', 'body', 'image']
+        fields = ['title', 
+        # 'title_tag',
+         'category',
+          'body',
+           'image']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            # 'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
              'body': forms.Textarea(attrs={'class': 'form-control',
                 'rows': 10, 
