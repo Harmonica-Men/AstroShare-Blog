a3a0597e8ee8af788d1f730eb597a2917ac31994
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-09-04 12:30:22 +0000

    Clean up code, insert comments, PEP8 for urls.py

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,28 +1,39 @@
 from django.urls import path, include
 from .views import (
-    FrontpageView, HomepageView, ArticleDetailView, AddPostView, UpdatePostView,
-    DeletePostView, LikeView, AddCommentView, search_view, ApodView, iss_location,
-    SubscribeView, ConfirmSubscription, AddCategoryView, CategoryView, CategoryListView,
-    CheckEmailView
-)
+    AddCategoryView, AddCommentView, AddPostView, ApodView, ArticleDetailView,
+    CategoryListView, CategoryView, CheckEmailView, ConfirmSubscription,
+    DeletePostView, FrontpageView, HomepageView, iss_location, LikeView,
+    SearchView, SubscribeView, UpdatePostView)
 
 urlpatterns = [
-    path('', HomepageView.as_view(), name= 'homepage'),
+    path('', HomepageView.as_view(), name='homepage'),
     path('add_newpost/', AddPostView.as_view(), name='add-newpost'),
-    path('add_newcategory/', AddCategoryView.as_view(), name='add-newcategory'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
-    path('frontpage/', FrontpageView.as_view(), name= 'frontpage-blogpost'),
-    path('article/edit/<int:pk>', UpdatePostView.as_view(), name= 'update-post'),
-    path('article/<int:pk>/delete', DeletePostView.as_view(), name= 'delete-post'),
-    path('like/<int:pk>', LikeView, name='like-post'),
-    path('article/<int:pk>/comment/', AddCommentView.as_view(), name= 'add-comment'),
-    path('category/<str:cats>/', CategoryView, name='category'),    
+    path(
+        'add_newcategory/', AddCategoryView.as_view(), name='add-newcategory'
+    ),
+    path(
+        'article/<int:pk>/', ArticleDetailView.as_view(),
+        name='article-detail'
+    ),
+    path('frontpage/', FrontpageView.as_view(), name='frontpage-blogpost'),
+    path(
+        'article/edit/<int:pk>/', UpdatePostView.as_view(), name='update-post'
+    ),
+    path(
+        'article/<int:pk>/delete/', DeletePostView.as_view(),
+        name='delete-post'
+    ),
+    path('like/<int:pk>/', LikeView, name='like-post'),
+    path(
+        'article/<int:pk>/comment/', AddCommentView.as_view(),
+        name='add-comment'
+    ),
+    path('category/<str:cats>/', CategoryView, name='category'),
     path('category_list/', CategoryListView, name='category-list'),
     path('search/', search_view, name='search'),
-    # path('nasa/', nasa_picture_of_the_day, name='nasa-picture-of-the-day'),
-    path('nasa/', ApodView.as_view(), name='nasa-picture-of-the-day'),   
+    path('nasa/', ApodView.as_view(), name='nasa-picture-of-the-day'),
     path('iss_location/', iss_location, name='iss-location'),
-    path('subscribe/', SubscribeView.as_view(), name='Subscribe'),
-    path('check-email/', CheckEmailView.as_view(), name='check_email'),
-    path('confirm/', ConfirmSubscription, name='ConfirmSubscription'),
+    path('subscribe/', SubscribeView.as_view(), name='subscribe'),
+    path('check-email/', CheckEmailView.as_view(), name='check-email'),
+    path('confirm/', ConfirmSubscription, name='confirm-subscription'),
 ]
