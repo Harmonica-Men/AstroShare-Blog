8f8207c896ced524e36fc8abeb98ea96c132c62a
Filip Van Elsande <filip.vanelslande@gmail.com>
2024-09-12 08:01:09 +0000

    Make urls.py PEP8 compliant

--- AstroShareBlog/urls.py
+++ AstroShareBlog/urls.py
@@ -1,30 +1,11 @@
-"""
-URL configuration for AstroShareBlog project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from blogger import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogger.urls')),
+    path('', include('blogger.urls')),  # Root URL includes blogger.urls
     path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-    path('blogger/', include('blogger.urls')),
-    path('summernote/', include('django_summernote.urls')),
+    path('members/', include('members.urls')),    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
